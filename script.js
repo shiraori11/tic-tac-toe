fd1e408566c3881f450455a8b8a9967cd4a976b4
shiraori11 <shiraori@proton.me>
2026-06-17 04:16:53 +0800

    Refactor game board Separated tic-tac-toe board from the playGame(), I made it a separated object instead. This is to make playGame only handles the logic and not store datas regarding the game.

--- script.js
+++ script.js
@@ -4,29 +4,34 @@ function gamePlayer(marker) {
   return {marker, announceWin};
 }
 
-gameInterface = (() => {
-  
-  const getUserChoice = () => {
-    const playerChoice = prompt("Send coordinates");
-  };
-  
-})();
-
-playGame = (() => {
-  let gameBoard = [
+const gameBoard = (() => {
+  let board = [
     ["n", "n", "n"],
     ["n", "n", "n"],
     ["n", "n", "n"],
   ];
 
+  const mark = (row, col, marker) => {
+    board[row][col] = marker;
+  }
+
+  const getBoard = () => board;
+
+  return {mark, getBoard};
+})();
+
+const playGame = (() => {
+
+
   const player1 = gamePlayer("X");
   const player2 = gamePlayer("O");
   let whoMove = player1;
-  let gameFinished = false;
+  let gameOngoing = true;
+  const board = gameBoard.getBoard();
 
 
   const addMarkerToBoard = (row, col) => {
-    gameBoard[row][col] = whoMove.marker;
+    gameBoard.mark(row, col, whoMove.marker);
     checkBoardWin(row, col, whoMove);
     if (whoMove == player1) {
       whoMove = player2;
@@ -42,26 +47,26 @@ playGame = (() => {
   };
 
   const checkDiagonal = (player) => {
-    const backSlashArr = [gameBoard[0][0], gameBoard[1][1], gameBoard[2][2]];
-    const forwardSlashArr = [gameBoard[0][2], gameBoard[1][1], gameBoard[2][0]];
+    const backSlashArr = [board[0][0], board[1][1], board[2][2]];
+    const forwardSlashArr = [board[0][2], board[1][1], board[2][0]];
 
-    if (checkEveryValue(backSlashArr, gameBoard[0][0])) {
+    if (checkEveryValue(backSlashArr, board[0][0])) {
       announceWhoWon(player)
-    } else if (checkEveryValue(forwardSlashArr, gameBoard[0][2])) {
+    } else if (checkEveryValue(forwardSlashArr, board[0][2])) {
       announceWhoWon(player)
     }
   };
 
   const checkRow = (row, player) => {
-    const rowArr = [gameBoard[row][0], gameBoard[row][1], gameBoard[row][2]];
-    if (checkEveryValue(rowArr, gameBoard[row][0])) {
+    const rowArr = [board[row][0], board[row][1], board[row][2]];
+    if (checkEveryValue(rowArr, board[row][0])) {
       announceWhoWon(player);
     };
   };
 
   const checkCol = (col, player) => {
-    const colArr = [gameBoard[0][col], gameBoard[1][col], gameBoard[2][col]];
-    if (checkEveryValue(colArr, gameBoard[0][col])) {
+    const colArr = [board[0][col], board[1][col], board[2][col]];
+    if (checkEveryValue(colArr, board[0][col])) {
       announceWhoWon(player);
     };
   };
@@ -72,19 +77,25 @@ playGame = (() => {
 
   const announceWhoWon = (player) => {
     player.announceWin();
-    gameFinished = true;
+    gameOngoing = false;
   };
 
-  const getGameBoard = () => gameBoard;
-  const isGameFinished = () => gameFinished;
+  const isOngoingGame = () => gameOngoing;
 
-  return {addMarkerToBoard, getGameBoard, isGameFinished};
+  return {addMarkerToBoard, isOngoingGame};
 })();
 
-// console.log(playGame.gameBoard);
-playGame.addMarkerToBoard(1, 2);
-playGame.addMarkerToBoard(0, 2);
-playGame.addMarkerToBoard(1, 1);
-playGame.addMarkerToBoard(2, 1);
-playGame.addMarkerToBoard(1, 0);
-console.log(playGame.getGameBoard());
+gameInterface = (() => {
+  
+  const getUserChoice = () => {
+    const getRow = prompt("Row: ");
+    const getCol = prompt("Col: ");
+
+    return [getRow, getCol];
+  };
+
+  const gameLoop = () => {
+    while()
+  }
+  
+})();
